c6f0454c663d93e469c54d28e139a02ea22f1d15
Pazhiloi <martinsen06@gmail.com>
2022-02-10 10:58:48 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-"use strict";
+const btn = document.getElementById('open-modal')
+ const modal = document.getElementById('modall') 
+ const closeBtn = document.querySelector('.modal__close')
+
+ btn.onclick = () => {
+   modal.classList.add('modal__active')
+
+   closeBtn.addEventListener('click', closeModal)
+
+   modal.addEventListener('click', hideModal)
+
+   function closeModal() {
+      modal.classList.remove('modal__active')
+      closeBtn.removeEventListener('click', closeModal)
+      modal.removeEventListener('click', hideModal)
+   }
+   function hideModal(event) {
+     if (event.target === modal) {
+       closeModal() 
+     }
+     
+   }
+ } 
+ 
+ 
